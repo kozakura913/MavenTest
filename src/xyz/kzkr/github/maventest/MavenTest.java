5847a2afab1966808ad50d5bb1481f2be6feb6e1
Kozakura913 <98575220+kozakura913@users.noreply.github.com>
2022-02-24 20:43:40 +0900

    基本Mavenプロジェクトの構成

---
+++ src/xyz/kzkr/github/maventest/MavenTest.java
@@ -0,0 +1,11 @@
+package xyz.kzkr.github.maventest;
+
+public class MavenTest{
+
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+	}
+	public static String helloworld() {
+		return "HelloWorld";
+	}
+}
